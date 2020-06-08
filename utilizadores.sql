e0dd4039561e559a058000cc9d8119e4977883b0
Simão Pedro <simao.s.pedro@gmail.com>
2020-06-08 22:06:46 +0100

    Implementação da view agregadora das informações sobre funcionários

--- utilizadores.sql
+++ utilizadores.sql
@@ -53,3 +53,27 @@ from funcionarioschefe fc
 	left join funcionarios fa
 		on fa.idfuncionario = av.idavaliado
 order by 1, 3, 4;
+
+-- View de informações de funcionário e descrição de função, departamento e tarefa
+create or replace view v_funcionarios_funcoes_departamentos_tarefas as
+select
+	f.idfuncionario as ID,
+    concat(f.primeiro, " ", f.apelido) as Nome,
+    f.cc, f.nif, f.niss,
+    date_format(f.dtanascimento, '%d-%m-%Y') as Data_Nasc,
+    concat(f.rua, ', ', f.numero, ', ', f.andar, ', ', f.fracao) as Morada,
+    f.localidade,
+    concat(f.numerocp, ' - ', f.extensaocp, ' ', f.designacaopostal) as Codigo_Postal,
+    date_format(f.dtacontratacao, '%d-%m-%Y') as Data_Contratacao,
+    fn.descricao as Funcao,
+    t.descricao as Tarefa
+from funcionarios f
+	join funcoes fn
+		on f.idfuncao = fn.idfuncao
+	join departamentos d
+		on fn.iddepartamento = d.iddepartamento
+	join listatarefas lt
+		on fn.idfuncao = lt.idfuncao
+	join tarefas t
+		on lt.idtarefa = t.idtarefa
+order by f.idfuncionario
