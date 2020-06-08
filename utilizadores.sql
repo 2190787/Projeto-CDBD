cc7b50c2b6a089b4464228f8952246bec80e18dd
Ricardo <2190787@my.ipleiria.pt>
2020-06-08 20:13:45 +0100

    Update script utilizadores
    Acrescentei View

--- utilizadores.sql
+++ utilizadores.sql
@@ -28,3 +28,28 @@ DROP USER funcionariochefe@localhost;
 FLUSH PRIVILEGES;
 CREATE USER 'funcionariochefe'@'localhost' IDENTIFIED BY 'funcionariochefe';
 SHOW GRANTS FOR funcionariochefe@localhost;
+
+-- View de avaliaçãoes atribuidas por cada funcionário chefe
+create or replace view v_avaliacoes_preenchidas_avaliadores as
+select
+	fc.idfuncionariochefe as 'Id Avaliador',
+	concat(f.primeiro, " ", f.apelido) as 'Funcionário Avaliador',
+    ka.ano as Ano,
+    ka.idavaliado as 'Id Funcionário',
+    concat(fa.primeiro, " ", fa.apelido) as 'Funcionário Avaliado',
+    ka.idkpi as 'Id Kpi',
+    k.descricao as 'Kpi avaliado',
+    ka.objetivo as Objetivo,
+    ka.avaliacao as Avaliação
+from funcionarioschefe fc
+	left join funcionarios f
+		on fc.idfuncionariochefe = f.idfuncionario
+	join avaliacoesdesempenho av
+		on av.idavaliador = fc.idfuncionariochefe
+	join kpisavaliados ka
+		on ka.ano = av.ano and ka.idavaliado = av.idavaliado
+	join kpis k
+		on k.idkpi = ka.idkpi
+	left join funcionarios fa
+		on fa.idfuncionario = av.idavaliado
+order by 1, 3, 4;
